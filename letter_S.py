4a254fc30bd744f01f6b67ae0c8c2d6d12ba7050
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-11-02 14:46:53 +0300

    Add files via upload
    Программа для отрисовывания буквы S с помощью Черепашки

---
+++ letter_S.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.shape('turtle')
+for x in range (2):
+    turtle.forward(50)
+    turtle.left(90)
+turtle.forward(50)
+for x in range (2):
+    turtle.right(90)
+    turtle.forward(50)
+    
