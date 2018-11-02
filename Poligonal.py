bf16d4783b006f67942fbe534f0a102cc1ce83cb
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-11-02 14:52:46 +0300

    Update Poligonal.py

--- Poligonal.py
+++ Poligonal.py
@@ -1,13 +1,13 @@
 import turtle
 turtle.shape('turtle')
-l=10
-y=2
-x=3
+l = 10
+y = 2
+x = 3
 def figure(name):
     turtle.penup()
     turtle.goto(y/2,2*y)
     turtle.pendown()
-    u=360/name
+    u = 360/name
     turtle.right(u)
     for x in range (name):
         turtle.forward(l)
@@ -15,9 +15,9 @@ def figure(name):
     turtle.left(u)
 for o in range (10):
     figure(x)
-    l+=10
-    y+=5
-    x+=1
+    l += 10
+    y += 5
+    x += 1
 
 
     
