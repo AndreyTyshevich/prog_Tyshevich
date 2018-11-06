d939142f53d201d293b49cdf741b6b955db65b9c
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-11-06 19:09:33 +0300

    Update and rename Spider.py to turtle/file_Spider.py

--- turtle/file_Spider.py
+++ turtle/file_Spider.py
@@ -3,12 +3,8 @@ turtle.shape('turtle')
 x = int(input())
 u = 180 - 360/x
 for y in range(x):
-    turtle.forward (50)
+    turtle.forward(50)
     turtle.stamp()
     turtle.left(180)
-    turtle.forward (50)
-    turtle.left(u)
-
-    
-    
-    
+    turtle.forward(50)
+    turtle.left(u) 
