b0d241d5e87e55c712f08f9c714a047f99cebdb2
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-11-06 18:49:55 +0300

    Update and rename letter_S.py to turtle/file_letter_S.py

--- turtle/file_letter_S.py
+++ turtle/file_letter_S.py
@@ -1,10 +1,9 @@
 import turtle
 turtle.shape('turtle')
-for x in range (2):
+for x in range(2):
     turtle.forward(50)
     turtle.left(90)
 turtle.forward(50)
-for x in range (2):
+for x in range(2):
     turtle.right(90)
     turtle.forward(50)
-    
