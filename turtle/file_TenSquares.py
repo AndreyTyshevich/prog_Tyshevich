20e5436a60bbdcf51354e73fd0c6a179b5363d0d
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-11-06 18:56:42 +0300

    Update and rename TenSquares.py to turtle/file_TenSquares.py

--- turtle/file_TenSquares.py
+++ turtle/file_TenSquares.py
@@ -1,14 +1,17 @@
 import turtle
 turtle.shape('turtle')
+
+
 def kv(l):
-    for i in range (4):
+    for i in range(4):
         turtle.forward(l)
         turtle.left(90)
 
-k=20
-for x in range (10):
+
+k = 20
+for x in range(10):
     kv(k)
-    k+=10
+    k += 10
     turtle.penup()
     turtle.right(90)
     turtle.forward(5)
@@ -16,4 +19,3 @@ for x in range (10):
     turtle.forward(5)
     turtle.right(180)
     turtle.pendown()
-    
