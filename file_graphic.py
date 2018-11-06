dc1e2a548962c8d57141279ee71c9dec061aca84
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-11-06 10:09:42 +0300

    Update file_graphic.py
    Изменены названия переменных

--- file_graphic.py
+++ file_graphic.py
@@ -3,6 +3,7 @@ import random
 
 window = gr.GraphWin("pic", 1500, 900)
 
+clouds = []
 
 def draw_picture():
     draw_background()
@@ -40,16 +41,16 @@ def draw_sun_face():
     draw_mouth(150, 260, 250, 260, 'black')
 
 
-def draw_mouth(x1, y1, x2, y2, c):
-    mouth = gr.Line(gr.Point(x1, y1), gr.Point(x2, y2))
+def draw_mouth(x_bottom, y_bottom, x_top, y_top, colour):
+    mouth = gr.Line(gr.Point(x_bottom, y_bottom), gr.Point(x_top, y_top))
     mouth.setWidth(20)
-    mouth.setOutline(c)
+    mouth.setOutline(colour)
     mouth.draw(window)
 
 
-def draw_eye(x, y, r, c):
+def draw_eye(x, y, r, colour):
     eye = gr.Circle(gr.Point(x, y), r)
-    eye.setFill(c)
+    eye.setFill(colour)
     eye.draw(window)
 
 
@@ -59,15 +60,15 @@ def draw_eye_pupil(x, y, r):
     pupil.draw(window)
 
 
-def draw_eyebrow(x1, y1, x2, y2):
-    eyebrow = gr.Line(gr.Point(x1, y1), gr.Point(x2, y2))
+def draw_eyebrow(x_bottom, y_bottom, x_top, y_top):
+    eyebrow = gr.Line(gr.Point(x_bottom, y_bottom), gr.Point(x_top, y_top))
     eyebrow.setWidth(10)
     eyebrow.setOutline('black')
     eyebrow.draw(window)
 
 
 def draw_sun_lines():
-    for x in range (1000):
+    for x in range(1):
         draw_sun_line(random.randint(50, 500), random.randint(50, 500))
         
 
@@ -80,13 +81,14 @@ def draw_sun_line(x, y):
 def draw_cloud():
     for x, y in (880, 250), (950, 250), (1020, 250), (910, 200), (980, 200):
         draw_small_cloud(x, y)
-
+        
 
 def draw_small_cloud(x, y):
     cloud = gr.Circle(gr.Point(x, y), 40)
     cloud.setFill('white')
     cloud.draw(window)
-
+    cloud_part = [cloud]
+    clouds.append(cloud_part)
 
 def draw_creature():
     face = gr.Circle(gr.Point(700, 500), 100)
@@ -102,11 +104,16 @@ def draw_creature():
     draw_leg(750, 620, 720, 550)
 
 
-def draw_leg(x1, y1, x2, y2):
-    leg = gr.Line(gr.Point(x1, y1), gr.Point(x2, y2))
+def draw_leg(x_bottom, y_bottom, x_top, y_top):
+    leg = gr.Line(gr.Point(x_bottom, y_bottom), gr.Point(x_top, y_top))
     leg.setWidth(20)
     leg.setOutline('pink')
     leg.draw(window)
+
+while True==True:
+    for cloud_part in clouds:
+        time.sleep(0.0001)
+        cloud_part.move(1.5,0)
 
 
 draw_picture()
