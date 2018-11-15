43ea3c29d019bc611e26e5f62a26c9f3bf34331a
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-11-15 21:48:18 +0300

    Update and rename gamewithcirclesplayableversion.py to file_game.py
    Убрана возможность получения более 1 очка с 1 шарика, согласовано с ПЕП8

--- file_game.py
+++ file_game.py
@@ -10,6 +10,7 @@ col = ['green', 'red', 'blue', 'yellow', 'pink', 'orange']
 points = 0
 antipoints = 0
 
+
 def tick():
     global xc, yc, rc, circle
     canv.delete(circle)
@@ -18,19 +19,20 @@ def tick():
     yc = random.randint(150, 550)
     rc = random.randint(20, 140)
     circle = canv.create_oval(xc - rc, yc - rc, xc + rc, yc + rc, fill=random.choice(col))
-    canv.create_text(500, 50, text = "Счёт:", font='Arial 25')
-    canv.create_text(500, 100, text = points, font='Arial 25')
-    canv.create_text(900, 50, text = "Промахи:", font='Arial 25')
-    canv.create_text(900, 100, text = antipoints, font='Arial 25')
-    root.after(1000, tick)
+    canv.create_text(500, 50, text="Счёт:", font='Arial 25')
+    canv.create_text(500, 100, text=points, font='Arial 25')
+    canv.create_text(900, 50, text="Промахи:", font='Arial 25')
+    canv.create_text(900, 100, text=antipoints, font='Arial 25')
+    root.after(1750, tick)
+
 
 def left_button(event):
-    global points, antipoints
+    global points, antipoints, xc, yc, rc
     if (event.x-xc)**2 + (event.y-yc)**2 <= rc**2:
         points += 1
+        rc = 0
     else:
         antipoints += 1
-        
 
 
 root.bind('<Button-1>', left_button)
