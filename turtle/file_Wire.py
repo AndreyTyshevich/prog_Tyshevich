f9858fe720bae566e9bbbc7b66262b42ea4de939
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-11-16 13:51:12 +0300

    Update file_Wire.py
    Убраны однобуквенные переменные и лишняя функция

--- turtle/file_Wire.py
+++ turtle/file_Wire.py
@@ -2,22 +2,16 @@ import turtle
 turtle.shape('turtle')
 
 
-def pruzhina1(vitok1):
-    for x in range(180):
+def wire(loop):
+    for side in range(180):
         turtle.right(1)
-        turtle.forward(vitok1)
-
-
-def pruzhina2(vitok2):
-    for x in range(180):
-        turtle.right(1)
-        turtle.forward(vitok2)
-
+        turtle.forward(loop)
+        
         
 turtle.penup()
 turtle.goto(-350, 0)
 turtle.left(90)
 turtle.pendown()
-for y in range(7):
-    pruzhina1(1)
-    pruzhina2(0.25)
+for number in range(7):
+    wire(1)
+    wire(0.25)
