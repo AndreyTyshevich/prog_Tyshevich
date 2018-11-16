6dbd68ed46876d14147049de4ffadd592ac5d2aa
AndreyTyshevich <43672780+AndreyTyshevich@users.noreply.github.com>
2018-11-16 12:50:13 +0300

    Update and rename file_game.py to file_game_catch_the_ball.py
    Теперь при промахе засчитывается только одно очко в категорию промахи

--- file_game_catch_the_ball.py
+++ file_game_catch_the_ball.py
@@ -9,10 +9,11 @@ col = ['green', 'red', 'blue', 'yellow', 'pink', 'orange']
 
 points = 0
 antipoints = 0
+minuspoint = 0
 
 
 def tick():
-    global xc, yc, rc, circle
+    global xc, yc, rc, circle, antipoints, minuspoint
     canv.delete(circle)
     canv.delete(ALL)
     xc = random.randint(150, 1250)
@@ -22,19 +23,22 @@ def tick():
     canv.create_text(500, 50, text="Счёт:", font='Arial 25')
     canv.create_text(500, 100, text=points, font='Arial 25')
     canv.create_text(900, 50, text="Промахи:", font='Arial 25')
+    if minuspoint > 0:
+        antipoints += 1
     canv.create_text(900, 100, text=antipoints, font='Arial 25')
-    root.after(1750, tick)
+    minuspoint = 0
+    root.after(500, tick)
 
 
 def left_button(event):
-    global points, antipoints, xc, yc, rc
+    global points, minuspoint, xc, yc, rc
     if (event.x-xc)**2 + (event.y-yc)**2 <= rc**2:
         points += 1
         rc = 0
     else:
-        antipoints += 1
-
-
+        minuspoint += 1
+        
+        
 root.bind('<Button-1>', left_button)
 circle = canv.create_oval(-100, 0, 0, 0)
 tick()
